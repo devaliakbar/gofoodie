738bb55e0646a41a7c0f3f120bb0f3a603743cc9
Ali <dev.aliakbar@gmail.com>
2021-03-02 10:12:33 +0530

    Started Ui of Profile Page

--- lib/features/settings/presentation/pages/profile.dart
+++ lib/features/settings/presentation/pages/profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gofoodie/core/res/app_resources.dart';
+import 'package:gofoodie/core/widgets/normal_text.dart';
 import 'package:gofoodie/features/settings/presentation/widgets/profile_appbar.dart';
+import 'package:gofoodie/features/settings/presentation/widgets/profile_settings_field.dart';
 
 class Profile extends StatelessWidget {
   static const String routeName = '/profile';
@@ -9,6 +12,48 @@ class Profile extends StatelessWidget {
     return Scaffold(
       appBar: ProfileAppBar(),
       backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                NormalText(
+                  "Change your Profile Settings",
+                  boldText: true,
+                  color: AppColors.black,
+                  size: FontSizes.fontSizeL,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileSettingsField(
+                  title: "Full Name",
+                  value: "Ali Akbar",
+                  onEditClick: () {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileSettingsField(
+                  title: "Email",
+                  value: "email@email.com",
+                  onEditClick: () {},
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ProfileSettingsField(
+                  title: "Password",
+                  value: "*********",
+                  onEditClick: () {},
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/settings/presentation/widgets/profile_settings_field.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'package:gofoodie/core/res/app_resources.dart';
+import 'package:gofoodie/core/services/size_config.dart';
+import 'package:gofoodie/core/widgets/normal_text.dart';
+
+class ProfileSettingsField extends StatelessWidget {
+  final String title;
+  final String value;
+  final Function onEditClick;
+
+  ProfileSettingsField(
+      {@required this.title, @required this.value, @required this.onEditClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      width: double.infinity,
+      height: SizeConfig.height(6.5),
+      decoration: BoxDecoration(
+        color: AppColors.lightGrey,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                NormalText(
+                  title,
+                  color: AppColors.black,
+                  size: FontSizes.fontSizeXS,
+                ),
+                NormalText(
+                  value,
+                  color: AppColors.black,
+                  size: FontSizes.fontSizeBSM,
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: onEditClick,
+            child: Container(
+              height: SizeConfig.height(5),
+              width: SizeConfig.height(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(6),
+                ),
+              ),
+              child: Icon(
+                Icons.edit_outlined,
+                color: AppColors.blue,
+                size: IconSizes.iconSizeM,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
